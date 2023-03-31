15fc8fc95ca0175f606a7956e6d78e6223916137
DAVIDK <dk6216720@gmail.com>
2023-03-31 18:17:52 -0400

    Disable operator buttons upon click to remove repeat presses; enable upon number button press; look into preventing color change if possible

--- javascript.js
+++ javascript.js
@@ -11,8 +11,8 @@ let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
 // Variables to represent number, operator, number
-let number1st;
-let number2nd;
+let number1;
+let number2;
 let operator;
 
 // Operate function
@@ -34,12 +34,80 @@ let operate = function (op, a, b) {
     return op (parseInt(a), parseInt(b));
 };
 
-let whatever = operate('+', '5', '10');
-
-console.log(whatever);
-
 // Basic HTML Calc (Done)
 
 // Create functions to populate calc display when numbers pressed
 // **Note: should be storing 'display value' in variable, for future use
+let numberChain = document.querySelector('.displayHistory');
+let display = document.querySelector('.display');
+let displayZero = 0;
+let displayValue = '';
+let buttonDigitsArray = [];
+buttonDigitsArray[0] = document.querySelector('[type="0"]');
+buttonDigitsArray[1] = document.querySelector('[type="1"]');
+buttonDigitsArray[2] = document.querySelector('[type="2"]');
+buttonDigitsArray[3] = document.querySelector('[type="3"]');
+buttonDigitsArray[4] = document.querySelector('[type="4"]');
+buttonDigitsArray[5] = document.querySelector('[type="5"]');
+buttonDigitsArray[6] = document.querySelector('[type="6"]');
+buttonDigitsArray[7] = document.querySelector('[type="7"]');
+buttonDigitsArray[8] = document.querySelector('[type="8"]');
+buttonDigitsArray[9] = document.querySelector('[type="9"]');
 
+for (let i = 0; i < buttonDigitsArray.length; i++) {
+    buttonDigitsArray[i].addEventListener('click', function () {
+        displayValue += i;
+        display.innerHTML = displayValue;
+        document.querySelector('[type="+"]').disabled = false;
+        document.querySelector('[type="-"]').disabled = false;
+        document.querySelector('[type="*"]').disabled = false;
+        document.querySelector('[type="%"]').disabled = false;
+    })
+};
+
+//Operator Buttons
+
+let plusButton = document.querySelector('[type="+"]');
+plusButton.addEventListener('click', function () {
+    numberChain.innerHTML = numberChain.innerHTML + displayValue + ' + ';
+    displayValue = '';
+    document.querySelector('[type="+"]').disabled = true;
+    document.querySelector('[type="-"]').disabled = true;
+    document.querySelector('[type="*"]').disabled = true;
+    document.querySelector('[type="%"]').disabled = true;
+});
+
+let subtractButton = document.querySelector('[type="-"]');
+subtractButton.addEventListener('click', function () {
+    numberChain.innerHTML = numberChain.innerHTML + displayValue + ' - ';
+    displayValue = '';
+    document.querySelector('[type="+"]').disabled = true;
+    document.querySelector('[type="-"]').disabled = true;
+    document.querySelector('[type="*"]').disabled = true;
+    document.querySelector('[type="%"]').disabled = true;
+});
+
+let multiplyButton = document.querySelector('[type="*"]');
+multiplyButton.addEventListener('click', function () {
+    numberChain.innerHTML = numberChain.innerHTML + displayValue + ' * ';
+    displayValue = '';
+    document.querySelector('[type="+"]').disabled = true;
+    document.querySelector('[type="-"]').disabled = true;
+    document.querySelector('[type="*"]').disabled = true;
+    document.querySelector('[type="%"]').disabled = true;
+});
+
+let divideButton = document.querySelector('[type="%"]');
+divideButton.addEventListener('click', function () {
+    numberChain.innerHTML = numberChain.innerHTML + displayValue + ' % ';
+    displayValue = '';
+    document.querySelector('[type="+"]').disabled = true;
+    document.querySelector('[type="-"]').disabled = true;
+    document.querySelector('[type="*"]').disabled = true;
+    document.querySelector('[type="%"]').disabled = true;
+});
+
+let computeButton = document.querySelector('[type="="]');
+computeButton.addEventListener('click', function () {
+
+})
