1a5fad1d4fdf94af7813a9f2e7a4a74f2387ccb2
DAVIDK <dk6216720@gmail.com>
2023-04-04 23:36:01 -0400

    Equation sign works but lastoperation screen not working correctly

--- javascript.js
+++ javascript.js
@@ -55,7 +55,10 @@ computeButton = document.getElementById('=');
 
 // Number Button Click Callback Function
 function appendDisplay(numbr) {
-    if (screenWasReset === true){
+    // To make number input after operand press clear previous currentOperationScreen
+    // Issue after = press, with it resetting = calculation
+    // Add operand undefined clause to try to fix
+    if (screenWasReset === true && previousOperand !== undefined){
         currentOperationScreen.textContent = '';
         currentOperationScreen.textContent += numbr;
         currentOperationScreen.textContent = parseFloat(currentOperationScreen.textContent);
@@ -82,7 +85,8 @@ numberButtons.forEach((button) =>
 // Operand Press Callback Function
 
 function onOperandPress(operandBtn){
-    console.log('                  ');
+    console.log('                ');
+    console.log(`${operandBtn.textContent}         `);
     console.log('Initial Values on Operand Press');
     console.log('ScreenWasReset ' + screenWasReset);
     console.log('operand1 ' + operand1);
@@ -91,7 +95,7 @@ function onOperandPress(operandBtn){
     console.log('currentOperationScreen.textContent' + currentOperationScreen.textContent);
     
     // If previous click was operand and you press an operand again, to change operand, but without performing computation
-    if (screenWasReset === true && currentOperationScreen.textContent !== ''){
+    if (screenWasReset === true && currentOperationScreen.textContent !== '' && operand1 !== undefined){
         previousOperand = operandBtn;
         lastOperationScreen.textContent = `${operand1} ${previousOperand}`;
 
@@ -144,3 +148,29 @@ function onOperandPress(operandBtn){
 // On Operand Click Eventlistener
 operatorButtons.forEach((button) => button.addEventListener('click', () => onOperandPress(button.textContent)));
 
+// = Button
+computeButton = document.querySelector('.compute-operator');
+computeButton.addEventListener('click', () => {
+    if(operand1 === undefined || previousOperand === undefined || screenWasReset === true){
+        return;
+    }
+    operand2 = currentOperationScreen.textContent;
+    operator = previousOperand;
+    operand1 = operate(operand1, operand2, operator);
+    currentOperationScreen.textContent = operand1;
+    operand1 = undefined;
+    operand2 = undefined;
+    previousOperand = undefined;
+    screenWasReset = true;
+    /* if(operand1 !== undefined){
+        lastOperationScreen.textContent = `${operand1} ${previousOperand} = ${operand1}`;
+    } */
+
+    console.log('                  ');
+    console.log('= Press');
+    console.log('ScreenWasReset ' + screenWasReset);
+    console.log('operand1 ' + operand1);
+    console.log('operand2 ' + operand2);
+    console.log('previousOperator ' + previousOperand);
+    console.log('currentOperationScreen.textContent' + currentOperationScreen.textContent);
+})
